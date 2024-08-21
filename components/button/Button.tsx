ca5176622fe1a0fe2229c546d956d637361e8e40
yeon <ohy9205@naver.com>
2024-08-21 23:41:02 +0900

    Button 컴포넌트 생성

---
+++ components/button/Button.tsx
@@ -0,0 +1,19 @@
+import styles from "./Button.module.css";
+
+type Props = {
+  text: string;
+  theme?: Theme;
+  size?: Size;
+};
+type Theme = "primary" | "dangerous";
+type Size = "sm" | "md" | "lg";
+
+const Button = ({ text, theme = "primary", size = "md" }: Props) => {
+  return (
+    <button className={`${styles.button} ${styles[theme]} ${styles[size]}}`}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
