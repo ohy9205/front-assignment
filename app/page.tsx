6ce5fc9d9329ea867a5e2476f50b3626f888e987
yeon <ohy9205@naver.com>
2024-08-22 00:23:31 +0900

    메인페이지에 Todo List링크 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <Link href={"./todo-list"} className={styles.link}>
+        Todo List🗒️
+      </Link>
+    </main>
+  );
 }
