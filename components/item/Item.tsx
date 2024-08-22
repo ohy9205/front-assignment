31f49f6df0db9d08b66ad342f08de594ba6f7a93
yeon <ohy9205@naver.com>
2024-08-22 19:38:32 +0900

    완료/진행 토글 이벤트 추가

--- components/item/Item.tsx
+++ components/item/Item.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { toggleCompletItem } from "@/apis/api";
 import { TodoItem } from "@/types/todoItem";
+import { useState } from "react";
 import Button from "../button/Button";
 import Checkbox from "../checkbox/Checkbox";
 import styles from "./Item.module.css";
@@ -8,24 +12,32 @@ type Props = {
 };
 
 const Item = ({ item }: Props) => {
+  const [state, setState] = useState(item);
+
+  const checkHandler = (item: TodoItem) => async () => {
+    const res = await toggleCompletItem(item);
+    setState(res);
+  };
+
   return (
-    <article key={item.id} className={styles.article}>
+    <article key={state.id} className={styles.article}>
       <header className={styles.header}>
         <div className={styles.titleWrapper}>
           <Checkbox
-            value={item.id}
-            label={<h2 className={styles.text}>{item.title}</h2>}
-            // checkHandler={async () => {
-            //   await toggleItem(item);
-            // }}
+            checked={state.isCompleted}
+            value={state.id}
+            checkHandler={checkHandler(state)}
           />
+          <label className={styles.text} htmlFor={state.id}>
+            {state.title}
+          </label>
         </div>
         <div className={styles.buttonWrapper}>
           <Button text="update" />
           <Button text="delete" theme="dangerous" />
         </div>
       </header>
-      <div className={styles.content}>{item.content}</div>
+      <div className={styles.content}>{state.content}</div>
     </article>
   );
 };
