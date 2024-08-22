921e2854f728ce7dc5c46d8a07b1a58bb3341936
yeon <ohy9205@naver.com>
2024-08-22 20:03:00 +0900

    create버튼 추가

--- components/list/List.tsx
+++ components/list/List.tsx
@@ -1,17 +1,23 @@
 import { getList } from "@/apis/api";
 import { TodoItem } from "@/types/todoItem";
+import Button from "../button/Button";
 import Item from "../item/Item";
 import styles from "./List.module.css";
 
 const List = async () => {
   const data = await getList();
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
 
   return (
     <div className={styles.list}>
-      {typeof data === "string"
-        ? data
-        : data?.map((item: TodoItem) => <Item item={item} />)}
+      <div className={styles.create}>
+        <Button text="create" />
+      </div>
+      <div className={styles.inner}>
+        {typeof data === "string"
+          ? data
+          : data?.map((item: TodoItem) => <Item item={item} />)}
+      </div>
     </div>
   );
 };
