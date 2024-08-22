9b2b71ea827e8d6da43cf7a3120c6d88b51fdf6a
yeon <ohy9205@naver.com>
2024-08-22 21:18:20 +0900

    radix Dialog에서 사용할 수 있도록 MyButton 수정

--- components/myButton/MyButton.tsx
+++ components/myButton/MyButton.tsx
@@ -1,29 +1,28 @@
-"use client";
+// "use client";
 
+import React from "react";
 import styles from "./MyButton.module.css";
 
 type Props = {
   text: string;
   theme?: Theme;
   size?: Size;
-  clickHandler?: () => void;
+  onClick?: () => void;
 };
 type Theme = "primary" | "dangerous";
-type Size = "sm" | "md" | "lg";
+type Size = "sm" | "md" | "full";
 
-const MyButton = ({
-  text,
-  clickHandler,
-  theme = "primary",
-  size = "md",
-}: Props) => {
-  return (
-    <button
-      onClick={clickHandler}
-      className={[styles.button, styles[theme], styles[size]].join(" ")}>
-      {text}
-    </button>
-  );
-};
+const MyButton = React.forwardRef<HTMLButtonElement, Props>(
+  ({ text, onClick, theme = "primary", size = "md" }: Props, ref) => {
+    return (
+      <button
+        ref={ref}
+        onClick={onClick}
+        className={[styles.button, styles[theme], styles[size]].join(" ")}>
+        {text}
+      </button>
+    );
+  }
+);
 
 export default MyButton;
