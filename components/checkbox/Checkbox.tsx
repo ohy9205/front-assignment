f45db90a864c8e1f1c3ae1e3c974d28133be9cd0
yeon <ohy9205@naver.com>
2024-08-22 18:50:50 +0900

    Checkbox에서 label분리

--- components/checkbox/Checkbox.tsx
+++ components/checkbox/Checkbox.tsx
@@ -3,14 +3,13 @@
 import styles from "./Checkbox.module.css";
 
 type Props = {
-  label: React.ReactNode;
   value: string;
   checked?: boolean;
 };
 
-const Checkbox = ({ label, checked = false, value }: Props) => {
+const Checkbox = ({ checked = false, value }: Props) => {
   return (
-    <div className={[styles.checkbox].join("")}>
+    <>
       <input
         type="checkbox"
         className={styles.input}
@@ -18,10 +17,7 @@ const Checkbox = ({ label, checked = false, value }: Props) => {
         defaultChecked={checked}
       />
       <span className={styles.span}></span>
-      <label className={styles.label} htmlFor={value}>
-        {label}
-      </label>
-    </div>
+    </>
   );
 };
 
